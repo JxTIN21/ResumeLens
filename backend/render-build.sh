24d0df9bbc920c550efb2dda43a89c1d2b5bb339
Jatin Srivastava <141351678+JxTIN21@users.noreply.github.com>
2025-06-27 13:53:43 +0530

    Update Render build script with sudo

--- backend/render-build.sh
+++ backend/render-build.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-apt-get update && apt-get install -y build-essential python3-dev
+# Update and install system packages
+sudo apt-get update
+sudo apt-get install -y build-essential python3-dev
 
+# Upgrade pip and install Python packages
 pip install --upgrade pip
 pip install --prefer-binary -r requirements.txt
